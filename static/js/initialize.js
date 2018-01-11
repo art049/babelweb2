54a16dd7e228980017446b752554b95faa88a6b9
Arthur Pastel <arthur.pastel@gmail.com>
2018-01-11 12:36:06 +0100

    Changing ws address to be able to use it under a different route (behind a reverse proxy fe)

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -42,7 +42,7 @@ function babelWebV2() {
     };
 
     function connect(socketWarper) {
-        socketWarper.socket = new WebSocket(`ws${location.protocol == 'https:' ? 's' : ''}://${location.host}/ws`)
+        socketWarper.socket = new WebSocket(`ws${location.protocol == 'https:' ? 's' : ''}://${location}ws`)
 
 	    socketWarper.socket.onerror = console.error;
 
